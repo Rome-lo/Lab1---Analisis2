7f5f39a364a28a423e07911045abec02ac556555
romeo <romeo@RL-Inspiron>
2021-03-24 14:53:44 -0600

    Lab 3 prueba test error part 2

--- src/test/java/com/laboratorio/test/TestClassOne.java
+++ src/test/java/com/laboratorio/test/TestClassOne.java
@@ -34,7 +34,7 @@ public class TestClassOne {
     }
     @Test
     public void TestSuma(){
-        System.out.println("Ejecutando Test suma()");
+        System.out.println("Ejecutando test suma()");
         Operaciones op = new Operaciones();
         op.setDato1(Double.parseDouble("10"));
         op.setDato2(Double.parseDouble("15"));
